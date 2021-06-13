108bd604f75247ed75a5c8bf1c988bc43ea51d87
prusichka <denxhuk.prusak@gmail.com>
2021-06-14 02:04:18 +0300

    firstCommit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+new Swiper(".slider ", {
+  loop: true,
+});
+new Swiper(".sliderTwo ", {
+  loop: true,
+});
+
+const burger = document.querySelector(".menu");
+const mobileMenu = document.querySelector("#toggle");
+
+burger.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let pagePos = window.scrollY;
+  document.body.classList.add("is-menu-open");
+  document.setAttribute("data-scroll", pagePos);
+
+  mobileMenu.style.display = "block";
+});
