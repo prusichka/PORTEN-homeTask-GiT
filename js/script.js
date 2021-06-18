d99ce4861cb3a5fc6f1f4fe084e797f227da1856
prusichka <denchuk.prusak@gmail.com>
2021-06-18 10:19:44 +0300

    commit perfect swiper

--- js/script.js
+++ js/script.js
@@ -1,22 +1,33 @@
 new Swiper(".slider ", {
   loop: true,
+  breakpoints: {
+    820: {
+      slidesPerView: 3,
+    },
+    620: {
+      slidesPerView: 2,
+    },
+  },
 });
 new Swiper(".sliderTwo ", {
   loop: true,
+  breakpoints: {
+    820: {
+      slidesPerView: 3,
+    },
+    620: {
+      slidesPerView: 2,
+    },
+  },
 });
 new Swiper(".slider3 ", {
   loop: true,
-});
-
-const burger = document.querySelector(".menu");
-const mobileMenu = document.querySelector("#toggle");
-
-burger.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  let pagePos = window.scrollY;
-  document.body.classList.add("is-menu-open");
-  document.setAttribute("data-scroll", pagePos);
-
-  mobileMenu.style.display = "block";
+  breakpoints: {
+    820: {
+      slidesPerView: 3,
+    },
+    620: {
+      slidesPerView: 2,
+    },
+  },
 });
